b523cef19e60b17a66ae4241fbc987d2eb16eea9
Danila Bespalov <danila.bespalov@gmail.com>
2025-09-05 15:57:40 +0700

    Split non-mt & mt builds and upgrade to emsdk 4.0.13
    Bazel machinery on emsdk got upgraded to bazel modules instead of
    WORKSPACE, so we have to use patched old emsdk until we upgrade tflite.

--- WORKSPACE
+++ WORKSPACE
@@ -108,13 +108,16 @@ http_archive(
     sha256 = "91f711089f73d385295246beec35a7b4302e1732f5d7406ee792065fea0a0b65",
     strip_prefix = "emsdk-4.0.3/bazel",
     url = "https://github.com/emscripten-core/emsdk/archive/refs/tags/4.0.3.tar.gz",
+    patches = [
+      "//tensorflow:emsdk_revision_list.patch"
+    ]
 )
 
 load("@emsdk//:deps.bzl", emsdk_deps = "deps")
 emsdk_deps()
 
 load("@emsdk//:emscripten_deps.bzl", emsdk_emscripten_deps = "emscripten_deps")
-emsdk_emscripten_deps(emscripten_version = "4.0.3")
+emsdk_emscripten_deps(emscripten_version = "4.0.13")
 
 load("@emsdk//:toolchains.bzl", "register_emscripten_toolchains")
 register_emscripten_toolchains()
--- build-wasm.sh
+++ build-wasm.sh
@@ -26,11 +26,11 @@ emar qsL libtflite_c.a `find bazel-bin/ -name '*.o'`
 echo "Building a monolithic object to check for duplicate symbols..."
 emcc -o libtflite_c.so -r libtflite_c.a -s LINKABLE
 
-echo "Building tflite SIMD for wasm..."
-./bazel build --config=wasm_simd -c opt //tensorflow/lite/core/c:c_api //tensorflow/lite/delegates/xnnpack:xnnpack_delegate
-emar qsL libtflite_c-simd.a `find bazel-bin/ -name '*.o'`
+echo "Building tflite MT for wasm..."
+./bazel build --config=wasm_mt -c opt //tensorflow/lite/core/c:c_api //tensorflow/lite/delegates/xnnpack:xnnpack_delegate
+emar qsL libtflite_c-mt.a `find bazel-bin/ -name '*.o'`
 echo "Building a monolithic object to check for duplicate symbols..."
-emcc -o libtflite_c-simd.so -r libtflite_c-simd.a -s LINKABLE
+emcc -o libtflite_c-mt.so -r libtflite_c-mt.a -s LINKABLE
 
 echo "Build finished, resulting artifacts:"
 ls -l libtflite_c*.a
